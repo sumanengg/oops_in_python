0c7894a2e15cecc9df8acd8e518cf4e0ea83bef6
sumanengg.sg <sumanengg.sg@gmail.com>
2025-06-15 17:48:15 +0530

    Factory pattern added

--- Python_Design_Pattern_Principles.py
+++ Python_Design_Pattern_Principles.py
@@ -107,3 +107,86 @@ instance1 = Singleton(10)
 instance2 = Singleton(20)
 
 print(instance1==instance2)  # True, both are the same instance
+
+# Thread-Safe Singleton Pattern
+from threading import Lock, Thread
+class ThreadSafeSingletonMeta(type):
+    __instance = {}
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        
+        with cls._lock:
+            if cls not in cls.__instance:
+                instance = super().__call__(*args, **kwargs)
+                cls.__instance[cls] = instance
+        return cls.__instance[cls]
+    
+class singleton_typesafe(metaclass=ThreadSafeSingletonMeta):
+    value: str = None
+
+    def __init__(self, value):
+        self.value = value
+
+def test_singleton_typesafe(value: str) -> None:
+    singleton_typesafe_ = singleton_typesafe(value)
+    print(singleton_typesafe_.value)
+
+# if __name__ == "__main__":
+#     process1 = Thread(target=test_singleton_typesafe, args=("Sumanta",))
+#     process2 = Thread(target=test_singleton_typesafe, args=("Raj",))
+#     process1.start()
+#     process2.start()
+
+# Factory Pattern:
+""" Simple Notiification service created using Factory Method Design Pattern: """
+
+from abc import ABC, abstractmethod
+
+# step:1 Create the Product Interface
+class Notification(ABC):
+
+    @abstractmethod
+    def send(self, msg, recipient):
+        pass
+
+# step:2 Create the Concrete Product
+
+class EmailNotification(Notification):
+    "Concreate implementation of email notification"
+
+    def send(self, msg: str, recipient: str) -> None:
+        print(f"Sending Email:  {msg} for {recipient}")
+
+class SmsNotification(Notification):
+    "Concrete implementation of email SMS"
+    def send(self, msg: str, recipient: str) -> None:
+        print(f"Sending Email:  {msg} for {recipient}")
+
+
+# step:3 Create the abstruct Creator:
+class NotificationService(ABC):
+
+    @abstractmethod
+    def create_notification(self):
+        pass
+
+    def send_notification(self, msg, recipient):
+        notification = self.create_notification()
+        return notification.send(msg, recipient)
+
+# step:4 Concreate Creator    
+class EmailNotificationService(NotificationService):
+
+    def create_notification(self):
+        return EmailNotification()
+    
+class SmsNotificationService(NotificationService):
+
+    def create_notification(self):
+        return SmsNotification()
+    
+if __name__ == "__main__":
+    print("Sending Email Notification to google@gmail.com")
+    email = EmailNotificationService()
+    email.send_notification('All Okay!!', 'google@gmail.com')
